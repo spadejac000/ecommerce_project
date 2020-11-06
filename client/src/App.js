46908c283b525923f32d14eb09f9584fd8a81bc2
Jacob Spade <spade.jacob@Yahoo.com>
2020-11-06 12:42:54 -0800

    lsdkjf

--- client/src/App.js
+++ client/src/App.js
@@ -43,8 +43,3 @@ function App() {
 
 export default App;
 
-// Original scripts for client/package.json
-// "start": "react-scripts start",
-// "build": "react-scripts build",
-// "test": "react-scripts test",
-// "eject": "react-scripts eject"
