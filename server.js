98b307c10650f04d6f58b91901bbf5aed284af62
Jacob Spade <spade.jacob@Yahoo.com>
2020-11-06 08:59:36 -0800

    setting up for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
 const items = require('./routes/api/items');
 const users = require('./routes/api/users');
@@ -14,6 +15,16 @@ app.use(bodyParser.json());
 app.use('/api/items', items)
 app.use('/api/users', users)
 app.use('/api/cart', cart)
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
