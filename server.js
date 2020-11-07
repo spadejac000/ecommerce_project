516838782f1215220f70a7e7609ac69d3d0645eb
Jacob Spade <spade.jacob@Yahoo.com>
2020-11-07 14:46:22 -0800

    hlj

--- server.js
+++ server.js
@@ -20,31 +20,32 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`))
 
-let distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {  
-  app.use(express.static(path.join(__dirname, 'client/build'))); 
-    app.get('*', (req, res) => {    
-      res.sendfile(path.join(__dirname = 'client/build/index.html'));  
-    }
-  )
-}
+// if(process.env.NODE_ENV === 'production') {  
+//   app.use(express.static(path.join(__dirname, 'client/build'))); 
+//     app.get('*', (req, res) => {    
+//       res.sendfile(path.join(__dirname = 'client/build/index.html'));  
+//     }
+//   )
+// }
 
 //build mode
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 //db config
 const db = require('./config/keys').mongoURI
 
 // connect to mongo
-mongoose
-  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
-    console.log('mongodb connected')
-  })
-  .catch(err => console.log(err))
+// mongoose
+//   .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+//   .then(() => {
+//     console.log('mongodb connected')
+//   })
+//   .catch(err => console.log(err))
+
+  // Changed to this line for Heroku
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
