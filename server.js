ae17bf2a235986a60c5afdf637384a24e6464020
Jacob Spade <spade.jacob@Yahoo.com>
2020-11-09 09:57:54 -0800

    dighi

--- server.js
+++ server.js
@@ -35,18 +35,9 @@ if(process.env.NODE_ENV === 'production') {
   )
 }
 
-//production mode
-// if(process.env.NODE_ENV === 'production') {  
-//   app.use(express.static(path.join(__dirname, 'client/build'))); 
-    
-//   app.get('*', (req, res) => {    
-//     res.sendfile(path.join(__dirname, 'client/build/index.html'));  
-//   })
-// }
-
 //build mode
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
+// app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+// })
 
 //db config
 const db = require('./config/keys').mongoURI
